b34fbf52874ee3a4bda34068b8c75128c441a4ca
MiloValenzuela <valenzuelagarza@gmail.com>
2025-06-09 16:50:37 -0600

    Improved User Input Fetching

--- code/01-getting-started/first-app/investment_calculator.go
+++ code/01-getting-started/first-app/investment_calculator.go
@@ -6,9 +6,23 @@ import (
 )
 
 func main() {
-	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+	const inflationRate = 2.5
+
+	investmentAmount, expectedReturnRate := 1000.0, 5.5
+	var years float64
+
+	fmt.Print("Investment amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Espected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, float64(years))
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
