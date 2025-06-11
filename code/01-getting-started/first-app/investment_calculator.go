f314116d25cca6c272c21f73ae6ab6bffd2aa0ed
MiloValenzuela <valenzuelagarza@gmail.com>
2025-06-10 20:57:22 -0600

    printF

--- code/01-getting-started/first-app/investment_calculator.go
+++ code/01-getting-started/first-app/investment_calculator.go
@@ -23,6 +23,8 @@ func main() {
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, float64(years))
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+	// outputs information
+	// fmt.Println("Future Value: ", futureValue)
+	fmt.Printf("Future Value: %v\nFuture Value (adjusted for Inflation):%v", futureValue, futureRealValue)
+	// fmt.Println("Future Value (adjusted for Inflation):", futureRealValue)
 }
